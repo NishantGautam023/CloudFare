f1b1880592ba1b0422e1f0674cbcc543d506df61
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-02-21 18:53:55 -0500

    Deployed worker to internet

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,19 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World! Nishant is here!');
+		console.log(request.body)
+		console.log(request.headers)
+
+		if(request.method === "GET") {
+			return Response.json({
+				"status": "ok",
+				"message": "You have sent a GET request"
+			})
+		} else {
+			return Response.json({
+				message: "You did not send a GET request",
+			})
+		}
+
 	},
 };
